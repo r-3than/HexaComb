38d5f2b02e44bfc2152df60e418d191e0f567161
Ethan Ray <ethan.gregory.ray@gmail.com>
2022-08-04 21:10:37 +0100

    scroll

--- myapp/lib/src/newplay/newplayscreen.dart
+++ myapp/lib/src/newplay/newplayscreen.dart
@@ -6,23 +6,14 @@ import 'package:flame/extensions.dart';
 import 'package:flame/game.dart';
 import 'package:flame/input.dart';
 import 'package:flame/sprite.dart';
-
-class DraggableComponent extends PositionComponent with Draggable {
-  // update and render omitted
-
-  Vector2 dragDeltaPosition;
-  bool get isDragging => dragDeltaPosition != null;
-
-  @override
-  bool onDragUpdate(int pointerId, DragUpdateInfo event) {
-    final localCoords = event.eventPosition.game;
-    position = localCoords - dragDeltaPosition;
-    return false;
-  }
-}
+import 'package:flutter/foundation.dart';
 
 class IsometricTileMapExample extends FlameGame
-    with MouseMovementDetector, VerticalDragDetector, HasDraggables {
+    with
+        MouseMovementDetector,
+        VerticalDragDetector,
+        HasDraggables,
+        HorizontalDragDetector {
   static const String description = '''
     Shows an example of how to use the `IsometricTileMapComponent`.\n\n
     Move the mouse over the board to see a selector appearing on the tiles.
@@ -33,6 +24,8 @@ class IsometricTileMapExample extends FlameGame
   static const scale = 2.0;
   static const srcTileSize = 32.0;
   static const destTileSize = scale * srcTileSize;
+  static Vector2 lastCords = Vector2(0, 0);
+  static Vector2 cameraCords = Vector2(500, 500);
 
   static const halfSize = true;
   static const tileHeight = scale * (halfSize ? 8.0 : 16.0);
@@ -51,8 +44,12 @@ class IsometricTileMapExample extends FlameGame
     double maxSide = min(size.x, size.y);
     camera.viewport = FixedResolutionViewport(Vector2.all(maxSide));
     final someVector = Vector2(500, 500);
+    debugPrint("HERE");
 
     camera.followVector2(someVector);
+
+    debugPrint(camera.position.toString());
+    debugPrint("HERE");
     final tilesetImage = await images.load('tiles$suffix.png');
     final tileset = SpriteSheet(
       image: tilesetImage,
@@ -91,15 +88,36 @@ class IsometricTileMapExample extends FlameGame
     );
   }
 
+  @override
   void onDragUpdate(int pointerId, DragUpdateInfo event) {
+    super.onDragUpdate(pointerId, event);
     final localCoords = event.eventPosition.game;
-    camera.followVector2(localCoords);
+    var delta = lastCords - localCoords;
+    delta = delta / 1.8;
+    lastCords = localCoords;
+    var newpos = cameraCords + delta;
+    camera.followVector2(newpos);
+    cameraCords = newpos;
+  }
+
+  @override
+  void onDragStart(int pointerId, DragStartInfo startPosition) {
+    super.onDragStart(pointerId, startPosition);
+    lastCords = startPosition.eventPosition.game;
+    debugPrint("START...");
+  }
+
+  @override
+  void onHorizontalDragStart(DragStartInfo startPosition) {
+    super.onHorizontalDragStart(startPosition);
+    lastCords = startPosition.eventPosition.game;
+    debugPrint("START2...");
   }
 
   @override
   void onMouseMove(PointerHoverInfo info) {
     final screenPosition = info.eventPosition.game;
-    camera.followVector2(screenPosition);
+    //camera.followVector2(screenPosition);
     final block = base.getBlock(screenPosition);
     selector.show = base.containsBlock(block);
     selector.position.setFrom(topLeft + base.getBlockRenderPosition(block));
